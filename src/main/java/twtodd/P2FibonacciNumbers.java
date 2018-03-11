d5a0d7a64e3e9e21a5f98f8dc0fb3b8886b133b8
TR3Y <trey.todd@gmail.com>
2018-03-10 20:40:33 -0800

    two down

---
+++ src/main/java/twtodd/P2FibonacciNumbers.java
@@ -0,0 +1,19 @@
+package twtodd;
+
+public class P2FibonacciNumbers {
+
+  public static void main(String[] args) {
+    int sum = 0;
+    int z;
+    int c;
+    for (int i = 1 , y = 2; y<4000000; i = z, y += c ) {
+      if (y % 2 == 0) {
+        sum += y;
+      }
+      c = i;
+      z = y;
+    }
+    System.out.println("Sum of even Fibonacci numbers until 4000000: " + sum);
+  }
+}
+
