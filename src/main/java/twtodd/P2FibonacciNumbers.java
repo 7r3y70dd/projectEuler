bd30a8680034d42e5c43f08f5a4ae63b2a9bb446
TR3Y <trey.todd@gmail.com>
2018-03-10 20:45:03 -0800

    mod: trim down

--- src/main/java/twtodd/P2FibonacciNumbers.java
+++ src/main/java/twtodd/P2FibonacciNumbers.java
@@ -4,9 +4,7 @@ public class P2FibonacciNumbers {
 
   public static void main(String[] args) {
     int sum = 0;
-    int z;
-    int c;
-    for (int i = 1, y = 2; y < 4000000; i = z, y += c) {
+    for (int i = 1, y = 2, z, c; y < 4000000; i = z, y += c) {
       if (y % 2 == 0) {
         sum += y;
       }
